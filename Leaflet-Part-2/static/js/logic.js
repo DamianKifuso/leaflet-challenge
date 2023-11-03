533bc17183416406317128b0983fe8d0961d611b
DamianKifuso <damian.mwangura@gmail.com>
2023-11-04 01:57:40 +0800

    my first commit for leaflet challenge

---
+++ Leaflet-Part-2/static/js/logic.js
@@ -0,0 +1,4 @@
+// Store our API endpoints as queryUrl and plateUrl
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+let plateUrl = "static/data/PB2002_boundaries.json"
+
